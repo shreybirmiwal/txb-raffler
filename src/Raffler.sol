eb750a5c1b37ae5c5dc92c9c58c420bbd4a86dbb
Sree Duggirala <sreevanthduggirala@gmail.com>
2024-10-23 00:30:09 -0500

    add visibility to methods

--- src/Raffler.sol
+++ src/Raffler.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 contract Raffler {
-    
     // Structure for storing all raffle data
     struct Raffle {
         uint8 x;
@@ -15,25 +14,25 @@ contract Raffler {
 
     // Constructor to initialize Ownable() and Chainlink-related variables
     constructor() {}
-    
+
     // Function to enter the raffle, adds participant to the array
-    // Add function visibility, arguments, and return type as needed 
-    function enter() {}
-    
+    // Add function visibility, arguments, and return type as needed
+    function enter() public {}
+
     // Function to start the raffle, only callable by the owner
     // Add function visibility, arguments, and return type as needed
     // Make sure that this is only called once per raffle
-    function start() {}
+    function start() public {}
 
     // Function to request a random number from the VRF Coordinator
     // Add function visibility, arguments, and return type as needed
-    function requestRandomNumber() {}
-    
+    function requestRandomNumber() public {}
+
     // Function to fulfill the random number request and determine the winner
-    function settleRaffle() {}
+    function settleRaffle() public {}
 
     // Function to withdraw the winnings
     // Add function visibility, arguments, and return type as needed
     // Make sure that only winner can withdraw
-    function withdraw() {}
+    function withdraw() public {}
 }
